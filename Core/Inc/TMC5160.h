90ab31c3cbc640d7fa81e1354e62caf2b9058774
EmbededIlya <ilyatim2018@mail.ru>
2023-09-12 20:46:46 +0300

    Feat: add the code

---
+++ Core/Inc/TMC5160.h
@@ -0,0 +1,120 @@
+/*
+ * TMC5160_STM32_HAL.h
+ *
+ *  Created on: Jul 1, 2023
+ *      Author: sam
+ */
+
+#ifndef LIBS_TMC5160_STM32_HAL_TMC5160_STM32_HAL_H_
+#define LIBS_TMC5160_STM32_HAL_TMC5160_STM32_HAL_H_
+#include "main.h"
+
+// Filter setting macros
+#define TMC5160_FIRST_ACCELERATION_LIMIT     0xFFFF // 2^16 - 1
+#define TMC5160_FIRST_VELOCITY_LIMIT         0xFFFFF  // 2^20 - 1
+#define TMC5160_MAX_ACCELERATION_LIMIT       0xFFFF // 2^16 - 1
+/*Attention for BELOW: Do not set 0 in positioning mode, even if V1=0!*/
+#define TMC5160_SECOND_DECELERATION_LIMIT    0xFFFF  // 2^16 - 1
+#define TMC5160_MAX_DECELERATION_LIMIT       0xFFFF  // 2^16 - 1
+/*Attention BELOW: Do not set 0 in positioning mode, minimum 10 recommend!*/
+#define TMC5160_STOP_VELOCITY_LIMIT          0x3FFFF  // 2^18 - 1
+#define TMC5160_MAX_VELOCITY_LIMIT           8388096  // 2^23 - 512
+
+
+// Registers
+typedef enum{
+	// GENERAL CONFIGURATION REGISTERS (0X00…0X0F)
+	GCONF			= 0x00,	// RW
+	GSTAT			= 0x01,	// R + WC
+	IFCNT			= 0x02, // R
+	SLAVECONF		= 0x03, // W
+	IOIN			= 0x04, // R
+	OUTPUT			= 0x04, // W --- In UART mode, SDO_CFG0 is an output.
+	X_COMPARE		= 0x05, // W
+	OTP_PROG		= 0x06, // W
+	OTP_READ		= 0x07, // R
+	FACTORY_CONF	= 0x08, // RW
+	SHORT_CONF		= 0x09, // W
+	DRV_CONF		= 0x0A, // W
+	GLOBALSCALER	= 0x0B, // W
+	OFFSET_READ		= 0x0C, // R
+	// VELOCITY DEPENDENT DRIVER FEATURE CONTROL REGISTER SET (0X10…0X1F)
+	IHOLD_IRUN		= 0x10, // W
+	TPOWERDOWN		= 0x11, // W
+	TSTEP			= 0x12, // R
+	TPWMTHRS		= 0x13, // W
+	TCOOLTHRS		= 0x14, // W
+	THIGH			= 0x15, // W
+	// RAMP GENERATOR MOTION CONTROL REGISTER SET (0X20…0X2D)
+	RAMPMODE		= 0x20, // RW
+	XACTUAL			= 0x21, // RW
+	VACTUAL			= 0x22, // R
+	VSTART			= 0x23, // W
+	A1				= 0x24, // W
+	V1				= 0x25, // W
+	AMAX			= 0x26, // W
+	VMAX			= 0x27, // W
+	DMAX			= 0x28, // W
+	D1				= 0x2A, // W
+	VSTOP			= 0x2B, // W
+	TZEROWAIT		= 0x2C, // W
+	XTARGET			= 0x2D, // RW
+	// RAMP GENERATOR DRIVER FEATURE CONTROL REGISTER SET (0X30…0X36)
+	VDCMIN			= 0x33, // W
+	SW_MODE			= 0x34, // RW
+	RAMP_STAT		= 0x35, // R + WC
+	XLATCH			= 0x36, // R
+	// ENCODER REGISTER SET (0X38…0X3C)
+	ENCMODE			= 0x38, // RW
+	X_ENC			= 0x39, // RW
+	ENC_CONST		= 0x3A, // W
+	ENC_STATUS		= 0x3B, // R + WC
+	ENC_LATCH		= 0x3C, // R
+	ENC_DEVIATION	= 0x3D, // W
+	// DRIVER REGISTER SET (0X6C…0X7F)
+	CHOPCONF		= 0x6C, // RW
+	COOLCONF		= 0x6D, // W
+	DCCTRL			= 0x6E, // W
+	DRV_STATUS		= 0x6F, // R
+	PWMCONF			= 0x70, // W
+	PWM_SCALE		= 0x71, // R
+	PWM_AUTO		= 0x72, // R
+	LOST_STEPS		= 0x73, // R
+}TMC5160_Regs;
+
+// RampMode
+typedef enum  {
+	/* using all A, D and V parameters */
+	Positioning = 0,
+	/* Velocity mode to positive VMAX (using AMAX acceleration) */
+	VelocityPositive = 1,
+	/* Velocity mode to negative VMAX (using AMAX acceleration) */
+	VelocityNegative = 2,
+	/* velocity remains unchanged, unless stop event occurs */
+	Hold = 3,
+	} RampModes;
+
+// Struct
+typedef struct{
+		SPI_HandleTypeDef        *spi;
+		GPIO_TypeDef           *GPIOx;
+		uint16_t				   CS;
+
+		//Configuration Acceleration and velocity
+			uint32_t max_speed;
+			uint32_t stop_speed;
+			uint32_t first_speed;
+			uint16_t first_acceleration;
+			uint16_t max_deceleration;
+			uint16_t second_deceleration;
+			uint16_t max_acceleration;
+			uint8_t ramp_mode;
+
+}TMC5160_HandleTypeDef;
+
+// Prototipes Functions
+HAL_StatusTypeDef TMC5160_WriteRegister(TMC5160_HandleTypeDef *htmc, TMC5160_Regs reg_addr, uint8_t data[]);
+HAL_StatusTypeDef TMC5160_ReadRegister(TMC5160_HandleTypeDef *htmc, TMC5160_Regs reg_addr, uint8_t data[]);
+HAL_StatusTypeDef TMC5160_Configuration(TMC5160_HandleTypeDef *htmc);
+
+#endif /* LIBS_TMC5160_STM32_HAL_TMC5160_STM32_HAL_H_ */
---
+++ Core/Src/TMC5160.c
@@ -0,0 +1,195 @@
+#include "TMC5160.h"
+
+
+HAL_StatusTypeDef TMC5160_WriteRegister(TMC5160_HandleTypeDef *htmc, TMC5160_Regs reg_addr, uint8_t data[]){
+	  //reg_addr - т.к. это на write, первый бит -> 1
+	  reg_addr |= 0b10000000;
+	  uint8_t buff[5];
+	  buff[0] = reg_addr;
+	  buff[1] = data[0];
+	  buff[2] = data[1];
+	  buff[3] = data[2];
+	  buff[4] = data[3];
+
+	  //Receive
+	  HAL_GPIO_WritePin(htmc->GPIOx,htmc->CS, GPIO_PIN_RESET);
+	  HAL_StatusTypeDef result = HAL_SPI_Transmit(htmc->spi, buff, 5, 100);
+	  HAL_GPIO_WritePin(GPIOA,GPIO_PIN_4, GPIO_PIN_SET);
+	  return result;
+}
+
+HAL_StatusTypeDef TMC5160_ReadRegister(TMC5160_HandleTypeDef *htmc, TMC5160_Regs reg_addr, uint8_t data[]){
+
+	  uint8_t buff[5];
+	  buff[0] = reg_addr;
+	  buff[1] = data[0];
+	  buff[2] = data[1];
+	  buff[3] = data[2];
+	  buff[4] = data[3];
+	  // sending buffer
+	  HAL_GPIO_WritePin(htmc->GPIOx,htmc->CS, GPIO_PIN_RESET);
+	  HAL_SPI_Transmit(htmc->spi, buff, 5, 100);
+	  HAL_StatusTypeDef result = HAL_SPI_TransmitReceive(htmc->spi, buff, data, 5, 100);
+	  HAL_GPIO_WritePin(htmc->GPIOx,htmc->CS, GPIO_PIN_SET);
+	  return result;
+}
+
+//  Configuration
+
+
+HAL_StatusTypeDef TMC5160_Configuration(TMC5160_HandleTypeDef *htmc){
+
+	uint8_t cmd_init[5][4]= {
+			{0x00, 0x01, 0x00, 0xC3},
+			{0x00, 0x06, 0x1F, 0x0A},
+			{0x00, 0x00, 0x00, 0x0A},
+			{0x00, 0x00, 0x00, 0x04},
+			{0x00, 0x00, 0x01, 0xF4},
+	};
+
+	HAL_StatusTypeDef result = TMC5160_WriteRegister(htmc, CHOPCONF, cmd_init[0]);
+
+	if(result != HAL_OK){
+			return result;
+	}
+
+	result = TMC5160_WriteRegister(htmc, IHOLD_IRUN, cmd_init[1]);
+
+	if(result != HAL_OK){
+				return result;
+	}
+
+	result = TMC5160_WriteRegister(htmc, TPOWERDOWN, cmd_init[2]);
+
+	if(result != HAL_OK){
+		return result;
+	}
+
+	result = TMC5160_WriteRegister(htmc, GCONF, cmd_init[3]);
+
+	if(result != HAL_OK){
+				return result;
+	}
+
+	result = TMC5160_WriteRegister(htmc,  TPWMTHRS, cmd_init[4]);
+
+	if(result != HAL_OK){
+				return result;
+	}
+	return result ;
+}
+
+// Drive functions
+HAL_StatusTypeDef TMC5160_setFirstAcceleration(TMC5160_HandleTypeDef *htmc, uint16_t value){
+if((value > TMC5160_FIRST_ACCELERATION_LIMIT) || (value < 0) ){
+		return HAL_ERROR;
+	}
+	htmc->first_acceleration = value;
+	return HAL_OK;
+}
+
+
+HAL_StatusTypeDef TMC5160_setMaxAcceleration(TMC5160_HandleTypeDef *htmc, uint16_t value){
+  if((value > TMC5160_MAX_ACCELERATION_LIMIT) || (value < 0) ){
+		return HAL_ERROR;
+	}
+	htmc->max_acceleration = value;
+	return HAL_OK;
+}
+//
+HAL_StatusTypeDef TMC5160_setMaxDeceleration(TMC5160_HandleTypeDef *htmc, uint16_t value){
+  if((value > TMC5160_MAX_DECELERATION_LIMIT) || (value < 0) ){
+		return HAL_ERROR;
+	}
+	htmc->max_deceleration = value;
+	return HAL_OK;
+}
+
+HAL_StatusTypeDef TMC5160_setSecondDeceleration(TMC5160_HandleTypeDef *htmc, uint16_t value){
+  if((value > TMC5160_SECOND_DECELERATION_LIMIT) || (value < 0) ){
+		return HAL_ERROR;
+	}
+	htmc->second_deceleration = value;
+	return HAL_OK;
+}
+
+HAL_StatusTypeDef TMC5160_setStopVelocity(TMC5160_HandleTypeDef *htmc, uint32_t value){
+  if((value > TMC5160_STOP_VELOCITY_LIMIT) || (value < 0) ){
+		return HAL_ERROR;
+	}
+	htmc->stop_speed = value;
+	return HAL_OK;
+}
+
+HAL_StatusTypeDef TMC5160_setFirstVelocity(TMC5160_HandleTypeDef *htmc, uint32_t value){
+  if((value > TMC5160_FIRST_VELOCITY_LIMIT) || (value < 0) ){
+		return HAL_ERROR;
+	}
+	htmc->first_speed = value;
+	return HAL_OK;
+}
+
+HAL_StatusTypeDef TMC5160_setMaxVelocity(TMC5160_HandleTypeDef *htmc, uint32_t value){
+  if((value > TMC5160_MAX_VELOCITY_LIMIT) || (value < 0) ){
+		return HAL_ERROR;
+	}
+	htmc->max_speed = value;
+	return HAL_OK;
+}
+
+HAL_StatusTypeDef TMC5160_setRampMode(TMC5160_HandleTypeDef *htmc, RampModes mode){
+ 	htmc->ramp_mode = mode;
+	return HAL_OK;
+}
+// Configuration Drive
+HAL_StatusTypeDef TMC5160_Configuration_Drive(TMC5160_HandleTypeDef *htmc){
+	TMC5160_setFirstAcceleration(htmc,1000);
+	TMC5160_setFirstVelocity(htmc,50000);
+	TMC5160_setMaxAcceleration(htmc,500);
+	TMC5160_setMaxVelocity(htmc,200000);
+	TMC5160_setMaxDeceleration(htmc,700);
+	TMC5160_setSecondDeceleration(htmc,1400);
+	TMC5160_setStopVelocity(htmc,10);
+	TMC5160_setRampMode(htmc, Positioning);
+}
+
+// Default Configurations
+HAL_StatusTypeDef TMC5160_default_init(TMC5160_HandleTypeDef *htmc){
+	HAL_StatusTypeDef result = TMC5160_Configuration(htmc);
+	if(result != HAL_OK){
+			return result;
+		}
+	result = TMC5160_WriteRegister(htmc, A1, htmc->first_acceleration);
+	if(result != HAL_OK){
+				return result;
+			}
+	result = TMC5160_WriteRegister(htmc, V1, htmc->first_speed);
+	if(result != HAL_OK){
+				return result;
+			}
+	result = TMC5160_WriteRegister(htmc, AMAX, htmc->max_acceleration);
+	if(result != HAL_OK){
+				return result;
+			}
+	result = TMC5160_WriteRegister(htmc, VMAX, htmc->max_speed);
+	if(result != HAL_OK){
+				return result;
+			}
+	result = TMC5160_WriteRegister(htmc, DMAX, htmc->max_deceleration);
+	if(result != HAL_OK){
+				return result;
+			}
+	result = TMC5160_WriteRegister(htmc, D1, htmc->second_deceleration);
+	if(result != HAL_OK){
+				return result;
+			}
+	result = TMC5160_WriteRegister(htmc, VSTOP, htmc->stop_speed);
+	if(result != HAL_OK){
+				return result;
+			}
+	result = TMC5160_WriteRegister(htmc, VSTOP, htmc->ramp_mode);
+	if(result != HAL_OK){
+				return result;
+			}
+	return HAL_OK;
+}
